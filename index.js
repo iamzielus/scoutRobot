799acb070087a3fbf2c9d73c06d45746abf80b8e
iamzielus <harcsailor@gmail.com>
2020-11-18 19:26:45 +0100

    Alpha 1.0.2
    working features
    {
        message said by bot (MOD COMMAND);
        kick & ban (MOD COMMAND);
        delete selected message by msg ID (MOD COMMAND);
        clear chat (MOD COMMAND);
        command list (MOD & USER COMMAND);
        embed with roles (reaction roles from other bot);
        co? miencho! feature (USER COMMAND);
        ping check (USER COMMAND);
        bot says what you want (USER COMMAND);
        fun comands for everyone - iq, gifs, custom commands;
    };
    
    to solve
    {
        bad words - needs more investigation;
        welcome message - solution: needs 24/7 hosting;
        rich presence - needs more data;
        nfsw gifs should not be there;
    };
    
    future plans
    {
        reaction roles;
        music bot;
    };

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const configFileName = './config.json';
 const file = require(configFileName);
 const {prefix, giphyToken, badWords, reactionMsgID} = require('./config.json');
 const client = new Discord.Client();
+const embedGif = new Discord.MessageEmbed()
 require('dotenv-flow').config();
+
+
 
 const config = {
   token: process.env.TOKEN,
@@ -29,17 +32,22 @@ client.on('ready', () => {
   });
 });
 
-//Welcome message
-
+//Welcome message (CANNOT BE DONE WITHOUT 24/7 HOSTING!!!)
+/*
 client.on('guildMemberAdd', member => {
-    let zasady = member.guild.channels.cache.find(ch => ch.name === 'zasady'); 
-    member.guild.channels.cache.find(ch => ch.name === 'powitalny').send({embed: { 
+    let zasady = member.guild.channels.cache.find(ch => ch.name === '📃regulamin📃');
+    let chRole = member.guild.channels.cache.find(ch => ch.name === '✏role✏'); 
+    member.guild.channels.cache.find(ch => ch.name === '👋dzień-dobry👋').send({embed: { 
         color: 10181046,
         title: "**Witaj!**",
         description: "**Czuwaj **" + `<@${member.user.id}>` + "**! Rozsiądź się wygodnie przy naszym ognisku!**",
         fields: [{
             name: "**REGULAMIN**",
-            value: `Aby zaakceptować regulamin przejdź na kanał ${zasady} i kliknij odpowiednią reakcję.`
+            value: `Aby zaakceptować regulamin przejdź na kanał ${zasady}, przeczytaj regulamin i postępuj zgodnie z dalszymi instrukcjami.`
+          },
+          {
+            name: "**ROLE**",
+            value: `Aby otrzymać odpowiednie role przejdź na kanał ${chRole} i kliknij odpowiednie reakcje.`
           }
         ],
         timestamp: new Date(),
@@ -49,9 +57,9 @@ client.on('guildMemberAdd', member => {
         }
         }});
 });
-
+*/
 //Message filtering
-
+/*
 client.on('message', async message => {
 
     let foundInText = false;
@@ -63,23 +71,25 @@ client.on('message', async message => {
         message.delete({ timeout: 100, reason: 'Słowa!' });
         message.channel.send('**Uważaj na słowa!** :rage:').then(msg => msg.delete({ timeout: 1000, reason: 'It had to be done.' }));
         return;
-    };
+    }; 
 });
-
+*/
 //Moderation commands
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(prefix) && !msg.guild && msg.author.bot) return;
-
+    if (msg.content.startsWith(prefix) && msg.guild && !msg.author.bot)
+    {
+    if (msg.member.hasPermission(['BAN_MEMBERS']))
+    {
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-
+    
     if(command === "say"){
         let text = args.join(" ");
         msg.delete();
         msg.channel.send(text);
     };
-
+    /*
     if(command === "reactmsg")
     {
       msg.delete();
@@ -112,6 +122,7 @@ client.on('message', msg => {
           });
         });
     }
+    */
   
     if (command === "kick") 
     {
@@ -130,7 +141,7 @@ client.on('message', msg => {
                   var totalResponses = response.data.length;
                   var responseIndex = Math.floor((Math.random()*10)+1) % totalResponses;
                   var responseFinal = response.data[responseIndex];
-                  msg.channel.send(":wave: " + member.displayName + ` **został wyrzucony z powodu: ${reason}.**`, {
+                  msg.channel.send(`${client.emojis.cache.find(emoji => emoji.name === 'weewoo')}` + member.displayName + ` **został wyrzucony z powodu: ${reason}.**`, {
                       files: [responseFinal.images.fixed_height.url]
                   })            }).catch(() => {
                       message.channel.send('**O nie! Wystąpił błąd przy wczytywaniu GIFa** :sob:');
@@ -171,7 +182,7 @@ client.on('message', msg => {
                   var totalResponses = response.data.length;
                   var responseIndex = Math.floor((Math.random()*10)+1) % totalResponses;
                   var responseFinal = response.data[responseIndex];
-                  msg.channel.send(":wave: " + member.displayName + ` **został zbanowany z powodu: ${reason}. Już nie wróci do naszego ogniska!**`, {
+                  msg.channel.send(`${client.emojis.cache.find(emoji => emoji.name === 'weewoo')}` + member.displayName + ` **został zbanowany z powodu: ${reason}. Już nie wróci do naszego ogniska!**`, {
                       files: [responseFinal.images.fixed_height.url]
                   })            }).catch(() => {
                       message.channel.send('**O nie! Wystąpił błąd przy wczytywaniu GIFa** :sob:');
@@ -197,7 +208,7 @@ client.on('message', msg => {
 
     if (command === 'clear') {
       const args = msg.content.split(' ').slice(1); // All arguments behind the command name with the prefix
-      const amount = args.join(' '); // Amount of messages which should be deleted
+      const amount = Number(args.join(' ')); // Amount of messages which should be deleted
 
       if (!amount) return msg.reply('Nie podałeś liczby wiadomości do usunięcia'); // Checks if the `amount` parameter is given
       if (isNaN(amount)) return msg.reply('Podaj prawidłową liczbę wiadomości'); // Checks if the `amount` parameter is a number. If not, the command throws an error
@@ -208,12 +219,29 @@ client.on('message', msg => {
         msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
         msg.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
       ).catch(console.error)});
+      msg.channel.send(`:recycle: **Usunięto ${amount} wiadomości.**`).then(message => {
+        message.delete({timeout:5000});
+      });      
+    };
+
+    if (command === 'censore')
+    {
+      let censoredID = msg.content.split(' ').slice(1);
+      msg.delete();
+      msg.channel.messages.fetch(`${censoredID}`)
+      .then(message => {
+      message.delete().then(msg.channel.send(`${client.emojis.cache.find(emoji => emoji.name === 'weewoo')} ***Ocenzurowano wiadomość.***`).then(info => {info.delete({timeout: 2000})}))
+      }).catch(err => {
+        msg.reply(`:x:***nie można usunąć wiadomości.***`).then(replyMsg => {replyMsg.delete({timeout:2000})});
+        console.error(err);
+      });
+
     };
 
     if(command === 'modhelp') {
       msg.channel.send({embed: {
        color: 15158332,
-       title: "**Komendy harcBOT'a**",
+       title: `${client.emojis.cache.get("715457208896323604")}**Komendy harcBOT'a**${client.emojis.cache.get("715457208896323604")}`,
        description: "**Lista komend dostępnych dla moderatorów**",
        fields: [{
            name: "**KOMENDY**",
@@ -230,6 +258,67 @@ client.on('message', msg => {
        }})
    };
 
+   if(command === 'rolesmsg') {
+    msg.channel.send({embed: {
+     color: 15158332,
+     title: `**Role dostępne na serwerze**`,
+     description: "**Kliknij na odpowiednią reakcję, aby otrzymać role.**",
+     fields: [{
+         name: "**Role**",
+         value: `:female_sign: - *kobiety*
+         :male_sign: - *mężczyzna*
+         :fleur_de_lis: - *ZHP*
+         :four_leaf_clover: - *ZHR*
+         :smiley_cat: - *poniżej 14 lat*
+         :joy_cat: - *14 - 16 lat*
+         :smirk_cat: - *16-18 lat*
+         :heart_eyes_cat: - *18+*
+         :japanese_goblin: - *gracz mafia*
+         :video_game: - *gracz Among Us* `,
+     }],
+     footer: {
+       text: "© Scout Robot 2020"
+     }
+     }})
+ };
+
+ if(command === 'regulamin') {
+  msg.delete();
+  msg.channel.send({embed: {
+   color: 10181046,
+   title: "**Regulamin**",
+   description: "**Zostaw reakcję, aby potwierdzić zapoznanie się z regulaminem.**",
+   fields: [{
+       name: "**OGÓLNE**",
+       value: `◈ Uczestników serwera obowiązuje przestrzeganie postanowień niniejszego regulaminu.
+       ◈ Discord jest platformą do wspólnej integracji widowni z Twórcą. Poza Regulaminem Społeczności tego serwera Użytkownik obowiązany jest przestrzegać również Warunków Usługi Discord (dostępnych jako ToS: https://discordapp.com/terms). 
+       ◈ Uczestniczących w serwerze obowiązuje się  ponadto przestrzeganie Prawa Harcerskiego.
+       ◈ Ostateczna interpretacja niniejszego regulaminu należy do administracji/właściciela serwera. 
+       ◈ Nieprzestrzeganie powyższych norm może doprowadzić do wydalenia takiej osoby z serwera. 
+       ◈ Administratorzy zastrzegają sobie możliwość dokonywania zmian w niniejszym regulaminie po jego opublikowaniu. 
+       ◈ Zmiany regulaminu oraz inne istotne decyzje administracja będzie ogłaszać na kanale "Informację"`,
+   },
+   {
+    name: "**ZASADY KANAŁÓW TEKSTOWYCH**",
+    value: `◈ Nakaz ograniczania używania wulgaryzmów na kanałach tekstowych a także głosowych. ( SZANUJMY SIEBIE NA WZAJEM, W KONCU KAZDY HARCERZ I HARCERKA TO WIELKA RODZINA)
+    ◈ Zakazane jest prowokowanie kłótni, dyskusji które mają negatywny wpływ na serwer. ( Wszystkie spory rozwiązujemy na kanale "skargi, zguby, zażalenia"
+    ◈ Zakaz wykorzystywania, oszukiwania i szantażowania innych użytkowników.
+    ◈ Zakaz obrażania graczy, administracji i serwera oraz działania na ich szkody.
+    ◈ Wszelkiego rodzaju błędy na serwerze należy zgłaszać do Administracji ( każdy popełnia błędy, my też mamy do tego prawo- grunt to je poprawiać :blush:)
+    ◈ Zabronione jest wysyłanie linków lub plików zawierających jakiekolwiek treści wulgarne/rasistowskie/pornograficzne itp.
+    ◈ Zakaz reklamowania zewnętrznych stron i innych serwerów - wyjątkiem jest zgoda Administracji ( <@&703370198740107266> , <@&703370367523094548> ) np. jak chodzi o zbiórkę na szczytne cele itd.`,
+}],
+   footer: {
+     text: "© Scout Robot 2020"
+   }
+   }}).then(message => {
+    message.react("✅").catch(console.error);
+   });
+};
+
+  }
+}
+  else return;
   });
   
 //User commands  
@@ -250,8 +339,8 @@ client.on('message', msg => {
           })
     };
 
-    if (!msg.content.startsWith(prefix) && msg.author.bot) return;
-
+    if (msg.content.startsWith(prefix) && !msg.author.bot)
+    {
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
@@ -268,9 +357,46 @@ client.on('message', msg => {
       msg.channel.send(`Użytkownik <@${user}> napisał: ${text}`);
     };
 
+    if(command === 'paulina')
+    {
+      msg.delete();
+      msg.channel.send('${client.emojis.cache.find(emoji => emoji.name === "popcornDS")} <@534774314369810454> **puścisz nam jakąś bajkę???** :pleading_face:')
+    };
+
+    if(command === 'hania')
+    {
+      msg.delete();
+      msg.channel.send('${client.emojis.cache.find(emoji => emoji.name === "rusParrot")} <@590272661087584357> **давайте сделаем чат более славянским.**')
+    };
+
     if(command === 'szymon')
     {
-      msg.channel.send('Szymuś to bardzo dobry kolega! :heart:')
+      msg.delete();
+      msg.channel.send(`${client.emojis.cache.find(emoji => emoji.name === "heygif")} <@332213072280289283> **to bardzo dobry kolega! :heart: (i mój beta tester! :heart_eyes:)**`)
+    };
+
+    if(command === 'majka')
+    {
+      msg.delete();
+      msg.channel.send(`${client.emojis.cache.find(emoji => emoji.name === 'pepeKing')} <@563398605159792642> **to *TOTALNY* szefc** :heart: :heart_eyes_cat:`)
+    };
+
+    if(command === 'author')
+    {
+      msg.delete();
+      msg.channel.send(`**${client.emojis.cache.find(emoji => emoji.name === "wumpusHype")} Autorem tego bota jest <@339111873641447424> Typ ma niezłą psychę, że mu się chciało! (serio, byłem momentami nieznośny i co chwilę wywalałem mu error do konsoli :))))**`)
+    };
+
+    if(command === 'klik')
+    {
+      msg.delete();
+      msg.channel.send(`${client.emojis.cache.find(emoji => emoji.name === 'wumpusKeySlam')}`)
+    };
+
+    if(command === 'dance')
+    {
+      msg.delete();
+      msg.channel.send(`${client.emojis.cache.find(emoji => emoji.name === 'wumpusDisco')}${client.emojis.cache.find(emoji => emoji.name === 'wumpusDisco')}${client.emojis.cache.find(emoji => emoji.name === 'wumpusDisco')}`)
     };
 
     if(command === 'gif')
@@ -282,12 +408,38 @@ client.on('message', msg => {
               var totalResponses = response.data.length;
               var responseIndex = Math.floor((Math.random()*10)+1) % totalResponses;
               var responseFinal = response.data[responseIndex];
-              msg.channel.send("**Oto twój losowy gif z: " + `${args}` + "!**", {
-                  files: [responseFinal.images.fixed_height.url]
-              })            }).catch(() => {
+              let obrazek = responseFinal.images.fixed_height.url;
+              msg.channel.send({embed: {
+                color: 10181046,
+                title: `**Oto twój losowy gif zawierający: ${args}!**`,
+                "image": {
+                  "url": obrazek
+                },
+                footer: {
+                  text: "© Scout Robot 2020"
+                },
+                }}
+              );
+                          }).catch(() => {
                   msg.channel.send('O nie! Wystąpił błąd przy wczytywaniu GIFa :sob:');
-              })
-
+              });
+              
+              
+/*
+{embed: {
+                color: 10181046,
+                title: "**Oto twój losowy gif**",
+                description: `**zawierający:` + ` ${args}` +`!**`,
+                fields: [{
+                    name: "**GIPHY**",
+                    value: [responseFinal.images.fixed_height.url]
+                  }
+                ],
+                footer: {
+                  text: "© Scout Robot 2020"
+                }
+                }}
+*/
     };
 
     //Funkcja losująca liczby
@@ -310,16 +462,36 @@ client.on('message', msg => {
           return msg.reply('Oznacz prawidłowego użytkownika!');
       }
       let member = msg.mentions.members.first();
-      var iq = randomInt(-200, 200);
+      if (member.user.id === '339111873641447424')
+      {
+      msg.channel.send(`<@${member.user.id}>` + "** posiada 200 IQ!**").catch(() => {
+      msg.channel.send('O nie! Wystąpił błąd przy wczytywaniu GIFa :sob:');
+      })  
+      }
+      else
+      {var iq = randomInt(-200, 200);
       msg.channel.send(`<@${member.user.id}>` + "** posiada **" + iq + "** IQ!**").catch(() => {
           msg.channel.send('O nie! Wystąpił błąd przy wczytywaniu GIFa :sob:');
+      }) } 
+    };
+
+    if(command === '.iq')
+    {
+      if (!msg.mentions.users.size) {
+          return msg.reply('Oznacz prawidłowego użytkownika!');
+      }
+      let member = msg.mentions.members.first();
+      msg.delete();     
+      msg.channel.send(`<@${member.user.id}>` + "** posiada 200 IQ!**").catch(() => {
+      msg.channel.send('O nie! Wystąpił błąd przy wczytywaniu GIFa :sob:');
       })  
+      
     };
 
     if(command === 'help') {
       msg.channel.send({embed: {
        color: 10181046,
-       title: "**Komendy harcBOT'a**",
+       title:`${client.emojis.cache.get("715457208896323604")}**Komendy harcBOT'a**${client.emojis.cache.get("715457208896323604")}`,
        description: "**Lista komend dostępnych dla każdego użytkownika**",
        fields: [{
            name: "**KOMENDY**",
@@ -334,6 +506,7 @@ client.on('message', msg => {
        }
        }})
    };
+  }
   
 });
 
